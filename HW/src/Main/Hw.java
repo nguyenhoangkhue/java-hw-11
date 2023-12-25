c4fa23db8f12b4a241ea331dfc4a7c2a516702e3
nguyenhoangkhue <snh110685@gmail.com>
2023-12-25 16:54:23 +0700

    upd

---
+++ HW/src/Main/Hw.java
@@ -0,0 +1,11 @@
+package Main;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Hw {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        Book book=new Book();
+
+    }
+}
